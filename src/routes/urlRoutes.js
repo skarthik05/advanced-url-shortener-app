92435dfd0386d680965a144202034c4ad1df27e1
karthik <karthiks0598@gmail.com>
2025-01-18 18:21:44 +0530

    feat(ratelimit): add middleware and update routes

--- src/routes/urlRoutes.js
+++ src/routes/urlRoutes.js
@@ -2,9 +2,10 @@ import express from 'express';
 import { createShortUrl } from '../controllers/urlController.js';
 import { redirectShortUrl } from '../controllers/urlController.js';
 import { logAnalytics } from '../controllers/analyticsController.js';
+import rateLimiter from '../middlewares/rateLimiter.js';
 const router = express.Router();
 
-router.post('/shorten', createShortUrl);
+router.post('/shorten',rateLimiter ,createShortUrl);
 router.get('/:alias', redirectShortUrl, logAnalytics);
 
 export default router;
