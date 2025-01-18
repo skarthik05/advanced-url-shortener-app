db23ff12233411cec4c911b4f245a22447232bd2
karthik <karthiks0598@gmail.com>
2025-01-18 18:21:44 +0530

    feat(ratelimit): add middleware and update routes

--- src/routes/analyticRoutes.js
+++ src/routes/analyticRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { getUrlAnalytics, getTopicAnalytics, getUserAnalytics } from '../controllers/analyticsController.js';
+import rateLimiter from '../middlewares/rateLimiter.js';
 const router = express.Router();
 
-router.get(`/overall`,getUserAnalytics)
+router.get(`/overall`, rateLimiter, getUserAnalytics)
 router.get('/:alias', getUrlAnalytics);
 router.get(`/topic/:topic`, getTopicAnalytics);
 
