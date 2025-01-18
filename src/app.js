1330c7a347b675642c613dc50c75a2f21cc6a5ba
karthik <karthiks0598@gmail.com>
2025-01-18 09:10:38 +0530

    revise(analytics): enhanced logAnalytics controller flow

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import session from 'express-session';
+import useragent from 'express-useragent';
 import passport from 'passport'
 import dotenv from 'dotenv';
 dotenv.config();
@@ -16,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
 app.use(passport.initialize());
 app.use(passport.session());
-
+app.use(useragent.express());
 app.use('/api/auth', authRoutes);
 app.use('/api/url', urlRoutes);
 
