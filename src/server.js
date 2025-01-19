b88f2717554c6c1013b7276a56d6386ad3a3e5ea
karthik <karthiks0598@gmail.com>
2025-01-19 11:14:17 +0530

    ci: add docker image

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import app from './app.js';
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on PORT:${PORT}`));
 
